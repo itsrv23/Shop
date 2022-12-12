d587a1f7e9d010a7bb693f4f452ccce3602bb8ad
Filimonov Sergey <serifil1282@gmail.com>
2022-12-12 17:48:32 +0500

    ResponseWrapperAds added

---
+++ src/main/java/ru/got/shop/model/dto/ResponseWrapperAds.java
@@ -0,0 +1,118 @@
+package ru.got.shop.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.annotation.Generated;
+import javax.validation.Valid;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * ResponseWrapperAds
+ */
+@lombok.AllArgsConstructor
+@lombok.Builder
+@lombok.NoArgsConstructor
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public class ResponseWrapperAds implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("count")
+    private Integer count;
+
+    @JsonProperty("results")
+    @Valid
+    private List<AdDto> results = null;
+
+    public ResponseWrapperAds count(Integer count) {
+        this.count = count;
+        return this;
+    }
+
+    /**
+     * Get count
+     *
+     * @return count
+     */
+
+    @Schema(name = "count", required = false)
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public ResponseWrapperAds results(List<AdDto> results) {
+        this.results = results;
+        return this;
+    }
+
+    public ResponseWrapperAds addResultsItem(AdDto resultsItem) {
+        if (this.results == null) {
+            this.results = new ArrayList<>();
+        }
+        this.results.add(resultsItem);
+        return this;
+    }
+
+    /**
+     * Get results
+     *
+     * @return results
+     */
+    @Valid
+    @Schema(name = "results", required = false)
+    public List<AdDto> getResults() {
+        return results;
+    }
+
+    public void setResults(List<AdDto> results) {
+        this.results = results;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ResponseWrapperAds responseWrapperAds = (ResponseWrapperAds) o;
+        return Objects.equals(this.count, responseWrapperAds.count) &&
+                Objects.equals(this.results, responseWrapperAds.results);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count, results);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class ResponseWrapperAds {\n");
+        sb.append("    count: ").append(toIndentedString(count)).append("\n");
+        sb.append("    results: ").append(toIndentedString(results)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
+
