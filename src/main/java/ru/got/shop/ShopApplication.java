491d1d5cce7d01f2ff721925ba6608152f4773dc
Filimonov Sergey <serifil1282@gmail.com>
2022-12-13 18:44:25 +0500

    Swagger header was corrected and moved to main class

--- src/main/java/ru/got/shop/ShopApplication.java
+++ src/main/java/ru/got/shop/ShopApplication.java
@@ -1,8 +1,19 @@
 package ru.got.shop;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(info = @Info(title = "Api-First-Approach Online-Shop-Application",
+        version = "v.Beta",
+        description = "Разработка по методу API FIRST командой \"GOT\"",
+        contact = @Contact(name = "Git repository", url = "https://github.com/itsrv23/Shop"),
+        license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")),
+        tags = @Tag(name = "Auth"))
 @SpringBootApplication
 public class ShopApplication {
 
