23a5166731e34d9d8fddda20c5edd0b157698c63
Filimonov Sergey <serifil1282@gmail.com>
2022-12-13 17:17:26 +0500

    code refactoring Junk generated code, deprecated methods and not used class Ads was deleted,

--- src/main/java/ru/got/shop/config/SwaggerConfig.java
+++ src/main/java/ru/got/shop/config/SwaggerConfig.java
@@ -9,8 +9,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(info = @Info(title = "Api-First-Approach Online-Shop-Application",
+        version = "v.Beta",
         description = "Разработка по методу API FIRST командой \"GOT\"",
-        contact = @Contact(name = "Gir repo", url = "https://github.com/itsrv23/Shop"),
+        contact = @Contact(name = "Git repository", url = "https://github.com/itsrv23/Shop"),
         license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")),
         servers = @Server(url = "http://localhost:8080"),
         tags = @Tag(name = "Auth"))
