3f7632cf9898dbda74a4e1052f105a84fe95f90a
itsrv23 <nifirus@gmail.com>
2022-12-17 14:07:37 +0300

    add new test IOException, Big Avatar

---
+++ src/main/java/ru/got/shop/exception/CustomIOException.java
@@ -0,0 +1,15 @@
+package ru.got.shop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class CustomIOException extends RuntimeException{
+    public CustomIOException() {
+        super("IOException, please repeat request again");
+    }
+
+    public CustomIOException(String message) {
+        super(message);
+    }
+}
