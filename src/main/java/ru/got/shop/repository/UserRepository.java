b3f9353c55f33d06ea4e774affbba989781540fd
itsrv23 <nifirus@gmail.com>
2022-12-21 22:51:55 +0300

    Criteria API /api/v2/users/filter

--- src/main/java/ru/got/shop/repository/UserRepository.java
+++ src/main/java/ru/got/shop/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package ru.got.shop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ru.got.shop.model.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findById(Integer id);
     Optional<User> findFirstByEmail(String email);
 }
