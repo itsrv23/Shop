0578768c6db232a04ba259e9d8c2a8ce098fbe58
Filimonov Sergey <serifil1282@gmail.com>
2022-12-22 23:49:09 +0500

    save to disk implementation

--- src/main/resources/liquibase/sql/40_ads_pictures.sql
+++ src/main/resources/liquibase/sql/40_ads_pictures.sql
@@ -8,6 +8,7 @@ CREATE TABLE ads_pictures
     uuid         uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     content_size BIGINT      NOT NULL,
     media_type   VARCHAR(50) NOT NULL,
-    file_bytes   oid         NOT NULL,
-    file_name    text
+    file_bytes   oid,
+    file_name    text        NOT NULL,
+    file_path    TEXT
 );
