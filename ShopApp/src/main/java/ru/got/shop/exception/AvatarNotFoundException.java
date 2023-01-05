0933b991cbccc10c49ade491b9084c7f96b2df94
Filimonov Sergey <serifil1282@gmail.com>
2023-01-06 00:44:55 +0900

    Request statuses that was above custom exceptions removed

--- ShopApp/src/main/java/ru/got/shop/exception/AvatarNotFoundException.java
+++ ShopApp/src/main/java/ru/got/shop/exception/AvatarNotFoundException.java
@@ -1,11 +1,7 @@
 package ru.got.shop.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import java.util.UUID;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class AvatarNotFoundException extends RuntimeException {
 
     public AvatarNotFoundException(UUID uuid) {
--- ShopApp/src/main/java/ru/got/shop/exception/ForbiddenException.java
+++ ShopApp/src/main/java/ru/got/shop/exception/ForbiddenException.java
@@ -1,9 +1,5 @@
 package ru.got.shop.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public class ForbiddenException extends RuntimeException {
     public ForbiddenException() {
         super("You Shall Not Pass (c) Gandalf");
